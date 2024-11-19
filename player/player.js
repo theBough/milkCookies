8368dd1c252872c924f330a65919e1edf3af9dd0
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-11-19 12:18:02 -0600

    Update player.js

--- player/player.js
+++ player/player.js
@@ -1,33 +1,49 @@
-function Player(x,y,w,h,img,speed){
+function Player(x, y, w, h, img, xSpeed, ySpeed) {
   this.x = x;
-  this.y= y;
+  this.y = y;
   this.w = w;
   this.h = h;
-  this.img = loadImage(img)
-  this.speed = speed;
-  
-  this.display = function(){
+  this.img = loadImage(img);
+  this.xSpeed = xSpeed;
+  this.ySpeed = ySpeed;
+
+  this.display = function () {
     this.img.resize(this.w, this.h);
     image(this.img, this.x, this.y);
-  }
-    this.update = function(){
-    if(keyIsDown(37)){
-      p.x -= this.speed;
+  };
+  this.update = function () {
+    if (keyIsDown(37)) {
+      if (!wallLeftRight()) {
+        this.xSpeed = -20;
+        this.ySpeed = 0;
+      }
     }
-     if(keyIsDown(39)){
-      p.x += this.speed;
+    if (keyIsDown(39)) {
+      if (!wallLeftRight()) {
+        this.xSpeed = 20;
+        this.ySpeed = 0;
+      }
     }
-     if(keyIsDown(38)){
-      p.y -= this.speed;
-    }
-    if(keyIsDown(40)){
-      p.y += this.speed;
-    }
-  }
-}//end player
 
+    if (keyIsDown(38)) {
+      if (!wallAboveBelow()) {
+        console.log("Please")
+        this.ySpeed = -20;
+        this.xSpeed = 0;
+      }
+    }
+    if (keyIsDown(40)) {
+      if (!wallAboveBelow()) {
+        this.ySpeed = 20;
+        this.xSpeed = 0;
+      }
+    }
+    p.x += this.xSpeed;
+    p.y += this.ySpeed;
+  };
+} //end player
 
-function everythingPlayer(){
-    p.display()
-    p.update()
+function everythingPlayer() {
+  p.display();
+  p.update();
 }
